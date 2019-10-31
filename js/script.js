40288c78644a9c4e508c225c1efe19ba4fcb62d7
RaghadHS <raghad.alsawalhi@gmail.com>
2019-10-31 11:27:24 +0200

    Addition of Comments

--- js/script.js
+++ js/script.js
@@ -4,22 +4,26 @@ var q1, q2, q3, q4, q5;
 var names ;
 
 
-
+//********************************** Function that asks for the name of user****************************
 function naming()
 {
-  names = window.prompt('Welcome !! What is your name ?');
+  names = prompt('Welcome !! What is your name ?');
 
 }
+//********************************** END OF FUNCTION****************************
 
+
+
+//********************************** Function for Asking User if they want to play****************************
 function askgame()
 {
-  game = window.prompt('Would you like to play a game?');
+  game = prompt('Would you like to play a game?');
   game = game.toLowerCase();
 
   // eslint-disable-next-line eqeqeq
   if ( (game == 'no') || (game == 'n'))
   {
-    window.prompt('Great to have you on my website,' + '  ' + names.toUpperCase()); 
+    prompt('Great to have you on my website,' + '  ' + names.toUpperCase()); 
   }
 
   // eslint-disable-next-line eqeqeq
@@ -27,33 +31,38 @@ function askgame()
   { prompting();
   }
 }
+//********************************** END OF FUNCTION****************************
 
+
+//********************************** Function for Prompting Questions****************************
 function prompting()
 {
-  q1 = window.prompt('I grew up in Jordan');
+  q1 = prompt('I grew up in Jordan');
   q1 = q1.toLowerCase(); //This answer is no.
   checkAnswer(q1,1);
 
-  q2 = window.prompt('I like to drink tea');
+  q2 = prompt('I like to drink tea');
   q2 = q2.toLowerCase(); //This answer is no.
   checkAnswer(q2,2);
 
-  q3 = window.prompt('I am tall');
+  q3 = prompt('I am tall');
   q3 = q3.toLowerCase(); //This answer is yes.
   checkAnswer(q3,3);
   
-  q4 = window.prompt('I do not like Molokhieh');
+  q4 = prompt('I do not like Molokhieh');
   q4 = q4.toLowerCase(); //This answer is yes.
   checkAnswer(q4,4);
 
-  q5 = window.prompt('I love listening to K-POP');
+  q5 = prompt('I love listening to K-POP');
   q5 = q5.toLowerCase(); //This answer is yes.
   checkAnswer(q5,5);
 
   goodbye(names);
-
 }
+//********************************** END OF FUNCTION****************************
 
+
+// Function to check the validy of the rentere response.
 function checkAnswer(ans , num) 
 { switch (num)
 {
@@ -103,21 +112,25 @@ case 5:
 
 default:
   break;
+}}
+//********************************** END OF FUNCTION****************************
 
-}
 
-}
-
+//********************************** Function for Goodbye Message****************************
 function goodbye(name1)
 {
-  window.prompt('Dont be a stranger,' + '  ' + name1 + '. Visit us again for another About Me quiz!');
+  prompt('Dont be a stranger,' + '  ' + name1 + '. Visit us again for another About Me quiz!');
 }
+//********************************** END OF FUNCTION****************************
 
-//set timer to 20 seconds
-//message to start game is shown after 7 seconds
+
+
+
+
+
+setTimeout(naming,3000);//Message asking for name is shown after 3 minutes
+
+setTimeout(askgame, 10000);//mMssage to start game is shown after 7 seconds
 //asking for the name of the user for the personalized message
-
-setTimeout(naming,3000);
-setTimeout(askgame, 10000);
 
 
